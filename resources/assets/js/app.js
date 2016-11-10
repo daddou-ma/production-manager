c0de82c9bdc774e951f238cc022e698706d995d5
Mohamed El Amine DADDOU <daddoumohamed31@gmail.com>
2016-11-10 18:01:03 +0100

    last 05

--- resources/assets/js/app.js
+++ resources/assets/js/app.js
@@ -21,6 +21,8 @@ import PulseLoader from 'vue-spinner/src/PulseLoader.vue'
 Vue.use(VeeValidate);
 Vue.use(VueRouter)
 
+Vue.http.options.root = 'http://gestion-pro.herokuapp.com/'
+
 var bus = new Vue();
 
 global.bus = bus;
